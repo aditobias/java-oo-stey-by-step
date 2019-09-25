fcca6de9e4af0ad5d892412d83e5e75dd8b13a69
Adrian Tobias <adrian.tobias@oocl.com>
2019-09-25 15:52:13 +0800

    Fixed practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,40 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private Integer number;
+    private Student leader;
+    private List<Student> studentList;
+
+    public Klass(Integer number){
+        this.number = number;
+        studentList = new ArrayList<>();
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName(){
+        return String.format("Class %d", number);
+    }
+
+    public void assignLeader(Student student){
+        if(this.studentList.contains(student)){
+            this.leader = student;
+        }else{
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student){
+        this.studentList.add(student);
+    }
+
+
+    public Student getLeader() {
+        return this.leader;
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,32 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(Integer id, String name, Integer age, Klass klass){
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(Integer id, String name, Integer age){
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        if(klass != null){
+            return super.introduce() + String.format(" I am a Teacher. I teach Class %d.", klass.getNumber());
+        }
+        return super.introduce() + " I am a Teacher. I teach No Class.";
+    }
+
+    public String introduceWith(Student student){
+        if(klass.getNumber().equals(student.getKlass().getNumber())){
+            return super.introduce() + String.format(" I am a Teacher. I teach %s.", student.getName());
+        }
+        return super.introduce() + String.format(" I am a Teacher. I don't teach %s.", student.getName());
+    }
 }
