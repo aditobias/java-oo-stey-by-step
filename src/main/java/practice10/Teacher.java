e1ae33bb13ed8a5915078de9a49f65131f4845a9
Adrian Tobias <adrian.tobias@oocl.com>
2019-09-25 15:54:44 +0800

    Fixed practice10

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,49 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+    private LinkedList<practice10.Klass> linkedList = new LinkedList<>();
+
+    public Teacher(Integer id, String name, Integer age, LinkedList<Klass> linkedList){
+        super(id, name, age);
+        this.linkedList = linkedList;
+    }
+
+    public Teacher(Integer id, String name, Integer age){
+        super(id, name, age);
+    }
+
+    public LinkedList<Klass> getClasses(){
+        return linkedList;
+    }
+
+    public String introduce(){
+        if(!linkedList.isEmpty()){
+            String klasses = "";
+            for(int i = 0; i < linkedList.size(); i++) {
+                if(i >= 1){
+                    klasses += ", ";
+                }
+                klasses += linkedList.get(i).getNumber().toString();
+            }
+
+            return super.introduce() + String.format(" I am a Teacher. I teach Class %s.", klasses);
+        }
+        return super.introduce() + " I am a Teacher. I teach No Class.";
+    }
+
+    public boolean isTeaching(Student student){
+        if(linkedList.contains(student.getKlass()) && student.isIn()){
+            return true;
+        }
+        return false;
+    }
+
+    public String introduceWith(Student student){
+        if(linkedList.contains(student.getKlass())){
+            return super.introduce() + String.format(" I am a Teacher. I teach %s.", student.getName());
+        }
+        return super.introduce() + String.format(" I am a Teacher. I don't teach %s.", student.getName());
+    }
 }
