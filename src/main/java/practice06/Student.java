a6e060deb01a66cd19f1a3ac4b637b40cf69fee9
Adrian Tobias <adrian.tobias@oocl.com>
2019-09-25 15:47:54 +0800

    Fixed practice06

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,19 @@
 package practice06;
 
-public class Student {
+public class Student extends Person{
+
+    private Integer klass;
+
+    public Student(String name, Integer age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        return super.introduce() + String.format(" I am a Student. I am at Class %d.", klass);
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private Integer klass;
+
+    public Teacher(String name, Integer age, Integer klass){
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, Integer age){
+        super(name, age);
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        if(klass != null){
+            return super.introduce() + String.format(" I am a Teacher. I teach Class %d.", klass);
+        }
+        return super.introduce() + " I am a Teacher. I teach No Class.";
+    }
 }
